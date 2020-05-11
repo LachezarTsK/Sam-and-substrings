1534286481b0cc794ff49f53adf73ec5bcfb0c90
Lachezar Kutsarov <lkutsarov@gmail.com>
2020-05-11 21:03:19 +0200

    Create Solution.java

---
+++ Solution.java
@@ -0,0 +1,46 @@
+package main.samAndSubstrings.secondStreamline;
+
+import java.util.Scanner;
+
+public class Solution {
+
+  private static int modulo = (int) Math.pow(10, 9) + 7;
+
+  public static void main(String[] args) {
+
+    Scanner scanner = new Scanner(System.in);
+    String int_inStringForm = scanner.next();
+    scanner.close();
+
+    int result = sum_of_allSubtrsings_modulo(int_inStringForm);
+    System.out.println(result);
+  }
+
+  /** Calculates the sum of all possible substrings, modulo Math.pow(10, 9) + 7. */
+  private static int sum_of_allSubtrsings_modulo(String int_inStringForm) {
+
+    int n = int_inStringForm.length();
+    char ch = int_inStringForm.charAt(n - 1);
+    int totalSum_modulo = n * 1 * Integer.parseInt(Character.toString(ch));
+
+    // Stores the sum of all members of a geormetric progression: first member=1, multiplier=10.
+    long sumGeomProg_firstMemberOne_multiplierTen = 1;
+
+    for (int i = n - 2; i >= 0; i--) {
+      sumGeomProg_firstMemberOne_multiplierTen =
+          (1 + sumGeomProg_firstMemberOne_multiplierTen * 10) % modulo;
+
+      ch = int_inStringForm.charAt(i);
+
+      // Other alternative, applying the ASCII: digit =  Character.toString(ch) - '0';
+      int digit = Integer.parseInt(Character.toString(ch));
+
+      long sumGeomProg = (digit * sumGeomProg_firstMemberOne_multiplierTen) % modulo;
+      long sum_currentDigit = ((i + 1) * sumGeomProg) % modulo;
+
+      totalSum_modulo = ((int) sum_currentDigit + totalSum_modulo) % modulo;
+    }
+
+    return totalSum_modulo;
+  }
+}
